0cd907e2f9b273ce1e918b6a9169f492f6f2ca4f
kanikama1122 <isseidive0506@icloud.com>
2024-10-24 05:15:20 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,27 @@
 # Streamlitライブラリをインポート
 import streamlit as st
+import random
 
-# ページ設定（タブに表示されるタイトル、表示幅）
-st.set_page_config(page_title="タイトル", layout="wide")
 
-# タイトルを設定
-st.title("自己啓発アプリ")
+def main():
+    st.title("今日の自己啓発アプリ")
+    st.write("名前を入力して自己啓発してください")
 
-# テキスト入力ボックスを作成し、ユーザーからの入力を受け取る
-user_input = st.text_input('あなたの名前を入力してください')
+name = st.text_input('あなたの名前を入力してください')
 
-# ボタンを作成し、クリックされたらメッセージを表示
-if st.button('挨拶する'):
-    if user_input:  # 名前が入力されているかチェック
-        st.success(f'🌟 こんにちは、{user_input}さん! 🌟')  # メッセージをハイライト
-    else:
-        st.error('名前を入力してください。')  # エラーメッセージを表示
+
 
 jikokeihatu =["スマホばかり見るな",
               "勉強しないとまずいって",
               "厳しいって",
               ]
-if st.button("自己啓発をする")
+if st.button("自己啓発をする"):
+    if name:
+        random.seed(name)
+        jikokeihatu =random.choice(jikokeihatu)
+        st.write({jikokeihatu})
+    else:
+        st.write("名前を入力してください")
+
+if __name__=="__main__":
+    main()            
